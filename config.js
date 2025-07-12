cdadde5312596c26778b41c0cbb52df50e391665
Funfart Games <funfartgames@gmail.com>
2025-07-12 12:38:46 -0400

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,8 @@ window.CONTRACT_ABI = [
 ];
 
 // ✅ Define your actual CIDs below
-window.CID_DEFAULT_1 = "ipfs://bafkreib43lx3zuxvszcf5o5c4rmfrybbvnkzvw4xsgvhid7rqbeipd6mnu";
-window.CID_DEFAULT_2 = "ipfs://bafkreihdwdceikckrbbokx7nwkkt2gf6m6xw3o4jygs32f4st4ihmuvmu4";
-window.CID_MERGED     = "ipfs://bafybeid37wbk2gnro4qcfhrhe3phzdqqra7txmtqmv5qajcujgmzzv6xju";
-window.CID_SENDING    = "ipfs://bafkreicpl36unqtzsn2vmz5pbgci6mjui7ylxtdjl5cn7pp7mdhd6ts7sm";
+window.CID_DEFAULT_1 = "ipfs://bafybeiclhpw4ila6u7oejt2zmezxrffl44dxjkxyfjbacawgcighlghr4i";
+window.CID_DEFAULT_2 = "ipfs://bafybeieiht7rijbauvain7fjz7ankjocrk4bz6d7vprqpscmuvfuevbmhy";
+window.CID_MERGED     = "ipfs://bafybeiayekrbt5rirlewyq6zfqx6r3ygwfsrwjqdmqohxcr5rmo5c2tadm";
+window.CID_SENDING    = "ipfs://bafybeidrzpoooah2mlnjmwhuvpg6o4vqqxsrgkgn3rwnqlsffttp5zrg4i";
+window.CID_GHOST    = "ipfs://bafybeigddxpm5fjmhxjmtwndbj7f47uuoknnzebpfvy4revunbggaaxeg4";
