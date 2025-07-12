6f2a6d49b58978adac7e3f974e1a79ff8ec6f090
Funfart Games <funfartgames@gmail.com>
2025-07-12 15:42:21 -0400

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ const sound = document.getElementById("teleport-sound");
 const toggleBtn = document.getElementById("teleport-toggle");
 
 // State order for toggling
-const stateOrder = ["CID_DEFAULT_1", "CID_DEFAULT_2", "CID_SENDING", "CID_MERGED", "CID_GHOST"];
+const stateOrder = ["CID_MERGED", "CID_SENDING", "CID_DEFAULT_2", "CID_GHOST"];
 let currentIndex = 0;
 
 let contract, signer;
@@ -23,10 +23,11 @@ const ipfsGateway = cid =>
 
 // Define all state CIDs
 const stateCIDs = {
-  CID_DEFAULT_1: window.CID_DEFAULT_1,
-  CID_DEFAULT_2: window.CID_DEFAULT_2,
+  //CID_DEFAULT_1: window.CID_DEFAULT_1,
+ 
   CID_MERGED: window.CID_MERGED,
-  CID_SENDING: window.CID_SENDING,
+  CID_SENDING: window.CID_SENDING, 
+  CID_DEFAULT_2: window.CID_DEFAULT_2,
   CID_GHOST: window.CID_GHOST
 };
 
@@ -86,8 +87,8 @@ function simulateTeleport(cidKey) {
 
       setTimeout(() => {
         overlay.classList.add("hidden");
-        simulateTeleport("CID_MERGED"); // Auto switch
-        currentIndex = stateOrder.indexOf("CID_MERGED");
+        simulateTeleport("CID_DEFAULT_2"); // Auto switch
+        currentIndex = stateOrder.indexOf("CID_DEFAULT_2");
       }, 1000); // Duration matches .gif / animation
     });
   } else {
