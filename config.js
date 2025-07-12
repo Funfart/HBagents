40b464f6a614e57e71e44c8f8dfa225cf59d97c0
Funfart Games <funfartgames@gmail.com>
2025-07-12 15:37:04 -0400

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-window.CONTRACT_ADDRESS = "0xb24D7Cd5eE9AcB56e5F1eA2986eB1a8133962407"; // Replace with real address
+window.CONTRACT_ADDRESS = "0xYourContractAddress"; // Replace with real address
 window.CONTRACT_ABI = [
   "function getTeleportState(uint256 tokenId) view returns (tuple(string currentCID, uint256 cooldownEndsAt, bool isOnlySender, bool isOnlyReceiver, bool hasRequest))",
   "function teleport(uint256 tokenId) external",
@@ -7,9 +7,8 @@ window.CONTRACT_ABI = [
   "event TeleportRequested(uint256 indexed tokenId)"
 ];
 
-//window.CID_DEFAULT_1 = "ipfs://bafybeiclhpw4ila6u7oejt2zmezxrffl44dxjkxyfjbacawgcighlghr4i";
-window.CID_DEFAULT_1 = "ipfs://bafybeiayekrbt5rirlewyq6zfqx6r3ygwfsrwjqdmqohxcr5rmo5c2tadm";
-//window.CID_MERGED     = "ipfs://bafybeigddxpm5fjmhxjmtwndbj7f47uuoknnzebpfvy4revunbggaaxeg4";
-window.CID_SENDING    = "ipfs://bafybeidrzpoooah2mlnjmwhuvpg6o4vqqxsrgkgn3rwnqlsffttp5zrg4i";
+window.CID_DEFAULT_1 = "ipfs://bafybeiclhpw4ila6u7oejt2zmezxrffl44dxjkxyfjbacawgcighlghr4i";
 window.CID_DEFAULT_2 = "ipfs://bafybeieiht7rijbauvain7fjz7ankjocrk4bz6d7vprqpscmuvfuevbmhy";
+window.CID_MERGED     = "ipfs://bafybeiayekrbt5rirlewyq6zfqx6r3ygwfsrwjqdmqohxcr5rmo5c2tadm";
+window.CID_SENDING    = "ipfs://bafybeidrzpoooah2mlnjmwhuvpg6o4vqqxsrgkgn3rwnqlsffttp5zrg4i";
 window.CID_GHOST      = "ipfs://bafybeigddxpm5fjmhxjmtwndbj7f47uuoknnzebpfvy4revunbggaaxeg4";
